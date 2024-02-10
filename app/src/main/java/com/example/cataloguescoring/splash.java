3d018ca8232a1f0930aaa509cd19dfe770832991
Abhishek Bairagi <abhishekbairagi2005@gmail.com>
2024-02-10 13:57:52 +0530

    splash screen

--- app/src/main/java/com/example/cataloguescoring/splash.java
+++ app/src/main/java/com/example/cataloguescoring/splash.java
@@ -1,5 +1,6 @@
 package com.example.cataloguescoring;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -13,12 +14,21 @@ public class splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(splash.this, uploadimg.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+
+        };
     }
-}
